86716b6ca0a02b6fd9f1830e338af6c3bace1f28
Rachel Cianfichi <cianfich@gmail.com>
2021-10-21 21:41:10 -0400

    Updated category post route to include proper body information.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,6 +50,9 @@ router.put('/:id', async(req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body,{
+      category_name: req.body.category_name
+    },
+    {
       where: {
         id: req.params.id
       }
