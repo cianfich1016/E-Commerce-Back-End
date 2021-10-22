008b5aedea5304ea9e41652eb9b67788bfaac082
Rachel Cianfichi <cianfich@gmail.com>
2021-10-21 21:43:41 -0400

    Updated body of post request in tag route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,7 +36,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async(req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -47,6 +49,9 @@ router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body,{
+      tag_name: req.body.tag_name
+    },
+    {
       where: {
         id: req.params.id
       }
